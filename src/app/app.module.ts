75f9e655b5ba03a5e178825431db44182f5b0c85
3284 <3284@wisseninfotech.com>
2019-10-31 19:29:25 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes, ActivatedRoute, ActivatedRouteSnapshot} from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EditAppComponent } from './edit-app/edit-app.component';
+import { DndModule } from 'ngx-drag-drop';
+import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
+
+import { SignaturePadModule } from 'angular2-signaturepad';
+
+const appRoutes: Routes = [
+  { path: '', component: EditAppComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EditAppComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    SweetAlert2Module.forRoot(),
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    DndModule,
+    SignaturePadModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
